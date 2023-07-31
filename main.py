1f5867e834d08581feca2f6820ccefe4508f7143
edwin <ramosaguilaredwin7@gmail.com>
2023-07-31 11:35:57 -0500

    programa

---
+++ main.py
@@ -0,0 +1,10 @@
+datos_personales = {}
+
+datos_personales['nombre'] = input('Escibe tu nombre: ')
+datos_personales['Apellido'] = input('Escibe tu apellido y nombre: ')
+datos_personales['dni'] = input('Escibe tu dni: ')
+datos_personales['edad'] = input('Escibe tu edad: ')
+datos_personales['sexo'] = input('Escibe tu sexo: ')
+
+print(datos_personales)
+##cresar una funcion 
